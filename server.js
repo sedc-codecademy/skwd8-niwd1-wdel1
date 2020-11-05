33430eb7feebcb2a5fee0b3350b266f4edbd311e
Igor Joshevski <gigo_j44@hotmail.com>
2020-11-05 20:39:32 +0100

    Adding HTTPS

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const setup = require('./setup');
 const express = require('express');
+const https = require('https'); //Native from nodejs API
+const fs = require('fs'); //Native from nodejs API (filesystem)
 const app = express(); //Server 1 
 const router = require('./consts/router.const');
 const cors = require('./consts/cors.const');
@@ -43,10 +45,24 @@ app.use('/api', router);
 
 
 app.all('/*', function(req, res, next) {
+	console.log(req.url)
     // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('static/index.html', { root: __dirname });
+    res.sendFile(`static/index.html`, { root: __dirname });
 });
 
+if(process.env.ENVIRONMENT == 'dev')
+{
+	let privateKey = fs.readFileSync(`../nodejs-ssl/nodejs-localhost.key`).toString(),
+	certificate = fs.readFileSync(`../nodejs-ssl/nodejs-localhost.crt`).toString();
+
+	let options = {key: privateKey, cert: certificate};
+
+	const httpsServer = https.createServer(options, app);
+
+	httpsServer.listen(8443, HOST, () => {
+		console.log('HTTPS Server running on port 8443');
+	});
+}
 app.listen(PORT, HOST, () => {
 	console.log(`Server is running on ${HOST}:${PORT}`);
 })
